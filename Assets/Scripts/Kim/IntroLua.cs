adcb7e0eec664a4d272666e5e6ebb5b368159799
kimajin0615 <120775596+kimajin0615@users.noreply.github.com>
2024-04-17 03:52:38 +0900

    Intro Scene

---
+++ Assets/Scripts/Kim/IntroLua.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+[CSharpCallLua]
+public delegate void GetGender(bool gender);
+
+public class IntroLua : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Kim/IntroSceneManager.cs
+++ Assets/Scripts/Kim/IntroSceneManager.cs
@@ -4,11 +4,26 @@ using UnityEngine.SceneManagement;
 
 public class IntroSceneManager : MonoBehaviour
 {
+    [SerializeField] public GameObject P_GenderQuestion;
     [SerializeField] public GameObject GenderQuestion;
     [SerializeField] public GameObject NameQuestion;
+    [SerializeField] public Image Standing;
+
+    [SerializeField] private Button p_genderSelectBtn;
     [SerializeField] private Button genderSelectBtn;
     [SerializeField] private Button nameSelectBtn;
     [SerializeField] private InputField nameInput;
+    [SerializeField] private Sprite male;
+    [SerializeField] private Sprite female;
+
+
+
+    public void P_MaleButton() { DataManager.Instance.gameData.playerGender = 0; }
+    public void P_FemaleButton() { DataManager.Instance.gameData.playerGender = 1; }
+    public void P_SecretButton() { DataManager.Instance.gameData.playerGender = 2; }
+    public void P_GenderSelect() { p_genderSelectBtn.interactable = true; }
+
+
 
     public void MaleButton() { DataManager.Instance.gameData.gender = false; }
     public void FemaleButton() { DataManager.Instance.gameData.gender = true; }
@@ -19,10 +34,24 @@ public class IntroSceneManager : MonoBehaviour
         SceneManager.LoadScene("HomeScene");
     }
 
+    public void TurnOnGenderQuestion()
+    {
+        P_GenderQuestion.SetActive(false);
+        GenderQuestion.SetActive(true);
+    }
+
     public void TurnOnNameQuestion()
     {
         GenderQuestion.SetActive(false);
         NameQuestion.SetActive(true);
+        if(DataManager.Instance.gameData.gender == false)
+        {
+            Standing.sprite = male;
+        }
+        else
+        {
+            Standing.sprite = female;
+        }
     }
 
     public void SetName()
